c9a54e5bdc7677540a8cdd3cd4234cef80646b74
Cristina <167357074+Christina0896@users.noreply.github.com>
2026-06-29 20:43:00 +0100

    Wrap app in toast provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Nunito_Sans, Lora } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from '../context/AuthContext';
+import { ToastProvider } from '../context/ToastContext';
 
 const nunitoSans = Nunito_Sans({
   variable: '--font-nunito-sans',
@@ -21,7 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${nunitoSans.variable} ${lora.variable}`}>
-        <AuthProvider>{children}</AuthProvider>
+        <ToastProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </ToastProvider>
       </body>
     </html>
   );
